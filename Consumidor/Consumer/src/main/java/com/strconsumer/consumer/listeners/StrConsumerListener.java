b3a3ce32397dcbe1459babb24204c363e279cdad
Amauri Junior <apojr98@gmail.com>
2022-07-24 17:27:12 -0300

    defining partitions

--- Consumidor/Consumer/src/main/java/com/strconsumer/consumer/listeners/StrConsumerListener.java
+++ Consumidor/Consumer/src/main/java/com/strconsumer/consumer/listeners/StrConsumerListener.java
@@ -3,6 +3,7 @@ package com.strconsumer.consumer.listeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,12 +11,20 @@ public class StrConsumerListener {
 	
 	private static Logger logger = LoggerFactory.getLogger(StrConsumerListener.class);
 	
-	@KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", 
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"0"})
+			}
+			, containerFactory = "strContainerFactory")
 	public void create(String message) {
 		logger.info("CREATE ::: Receive message {}", message);
 	}
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", 
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"1"})
+			}
+			, containerFactory = "strContainerFactory")
 	public void log(String message) {
 		logger.info("LOG ::: Receive message {}", message);
 	}
